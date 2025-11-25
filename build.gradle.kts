9da5dbf24ec5463ad4fd41f3847c8313a6167f3a
theparasol <serio0113112@gmail.com>
2025-11-25 23:04:33 +0100

    Added sonarqube

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,25 @@
+import org.sonarqube.gradle.SonarExtension
+
 plugins {
     alias(libs.plugins.protobuf) apply false
+    alias(libs.plugins.sonarqube) apply false
 }
 
 allprojects {
     apply(plugin = "java")
     apply(plugin = "java-library")
     apply(plugin = "idea")
+    apply(plugin = "org.sonarqube")
 
     extensions.configure<JavaPluginExtension> {
         toolchain.languageVersion.set(JavaLanguageVersion.of(rootProject.libs.versions.java.get()))
+    }
+
+    extensions.configure<SonarExtension> {
+        properties {
+            property("sonar.projectKey", "Ranked-Project_RankedProject")
+            property("sonar.organization", "ranked-project")
+        }
     }
 }
 
